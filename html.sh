f6a2f96e4607244679d5b6ac89e2837c275c83ab
xiejie <214533579@qq.com>
2014-11-09 07:08:51 -0500

    html.sh

--- html.sh
+++ html.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
+umask 022
+time=$(date +%m%d%H%M)
 for i in *.html */*.html 
 do
-    echo $i;
+    sed -i.bak -e "s|\(<.*src=.*\)\.js\(.*>\)|\1\.js\?time=$time\2|g" \
+               -e "s|\(<.*href=.*\)\.css\(.*>\)|\1\.css\?time=$time\2|g" $i
 done
