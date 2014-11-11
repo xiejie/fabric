93bc5632c7508ecf3e0ca822ae7297b53351db47
xiejie <214533579@qq.com>
2014-11-11 13:41:10 +0800

    html.sh

--- html.sh
+++ html.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 umask 022
 time=$(date +%m%d%H%M)
-for i in *.html */*.html 
+for view in `find . -name View -a -type d` 
 do
-    sed -i.bak -e "s|\(<.*src=.*\)\.js\(\?.*\)\{0,1\}\(['\"]\)\(.*>\)|\1\.js\?time=$time\3\4|g" \
-               -e "s|\(<.*href=.*\)\.css\(\?.*\)\{0,1\}\(['\"]\)\(.*>\)|\1\.css\?time=$time\3\4|g" $i
+    for html in $view/*.html $view/*/*.html 
+    do
+        sed -i.bak -e "s|\(<.*src=.*\)\.js\(\?.*\)\{0,1\}\(['\"]\)\(.*>\)|\1\.js\?time=$time\3\4|g" \
+                   -e "s|\(<.*href=.*\)\.css\(\?.*\)\{0,1\}\(['\"]\)\(.*>\)|\1\.css\?time=$time\3\4|g" $html
+    done
 done
