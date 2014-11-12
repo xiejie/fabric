f7c2a19989e871b698ed1225f3d07fefbdc88140
xiejie <214533579@qq.com>
2014-11-12 16:21:44 +0800

    fabfile.py

--- fabfile.py
+++ fabfile.py
@@ -21,7 +21,7 @@ env.hosts=['localhost']
 env.password='123456'
 
 def test():
-    local('echo hello')
+    local('./html.sh')
 
 def debug(web_code='SilverAge'):
     with lcd('/tmp'):
@@ -30,7 +30,7 @@ def debug(web_code='SilverAge'):
         else:
             print 'not exists'
 
-def sample_deploy(web_code,svn_path,conf,web_path):
+def campaign_deploy(web_code,svn_path,conf,web_path):
     # get web_code
     with lcd("/tmp/"):
         if local_exists(web_code).succeeded:
@@ -45,35 +45,55 @@ def sample_deploy(web_code,svn_path,conf,web_path):
         local("tar -czf %s.tar.gz %s/" % (web_code,web_code))
         put("%s.tar.gz" % web_code,"/tmp/%s.tar.gz" % web_code)
         local("rm -f %s.tar.gz" % web_code)
-    with cd(web_path):
-        if exists(web_code):
-            backup(web_code)
+    # with cd(web_path):
+    #     if exists(web_code):
+    #         backup(web_code)
     with cd('/tmp'):
         run("tar -zxf %s.tar.gz -C %s" % (web_code,web_path))
         run("rm -f %s.tar.gz" % web_code)
     with cd("%s/%s" % (web_path,web_code)):
         clearRuntime()
 
-@hosts('root@10...')
-def deploy():
-    # sample_deploy(web_code,svn_path,conf,web_path)
+def web_deploy(web_code,svn_path,conf,web_path):
+    # get web_code
+    with lcd("/tmp/"):
+        if local_exists(web_code).succeeded:
+            with lcd('/tmp/%s' % web_code):
+                update()
+        else:
+            local('git svn clone svn://10.1.0.241/%s %s' % (svn_path,web_code))
 
+    with lcd('/tmp/%s' % web_code):
+        chconfig(conf)
+        local("tar -czf %s.tar.gz * --exclude ThinkPHP --exclude Runtime" % web_code)
+        put("%s.tar.gz" % web_code,"/tmp/%s.tar.gz" % web_code)
+        local("rm -f %s.tar.gz" % web_code)
+    with cd('/tmp'):
+        run("tar -zxf %s.tar.gz -C %s" % (web_code,web_path))
+        run("rm -f %s.tar.gz" % web_code)
+    with cd("%s" % web_path):
+        clearRuntime()
 
-@hosts('root@10...')
-def wechat_deploy():
-    # sample_deploy(web_code,svn_path,conf,web_path)
-
-@hosts('root@10...')
+@hosts('root@10.')
 def prepare_deploy():
-    # sample_deploy(web_code,svn_path,conf,web_path)
-    pass
+    # web_deploy(web_code,svn_path,conf,web_path):
+    web_deploy('blinq','web_code/blinq_mobile','product','/php')
+
+@hosts('root@10...')
+def online_deploy():
+    campaign_deploy('admin','web_code/专题页面/admin','product','/online')
 
 @hosts('root@10...')
 def test_deploy():
-    # sample_deploy(web_code,svn_path,conf,web_path)
+    # campaign_deploy(web_code,svn_path,conf,web_path)
+    campaign_deploy('SilverAge','web_code/专题页面/SilverAge','uat','/www')
     # with cd('/www/SilverAge/Public/js'):
     #     run("sed -i \"s|\(.*\)/cp/\(.*\)|\\1/cptest/\\2|\" share.js")
 
+@hosts('root@1...')
+def wechat_deploy():
+    # campaign_deploy(web_code,svn_path,conf,web_path)
+    campaign_deploy('SilverAge','web_code/专题页面/SilverAge','product','/php/campaign')
 
 def update():
     # remove changed files, but keep unversioned files.
@@ -87,9 +107,10 @@ def backup(web_code):
     run("tar -czf %s-%s.tar.gz %s" % (web_code,DATE_FORMAT,web_code))
 
 def chconfig(conf='uat'):
-    if (conf != 'uat'):
-        local("sed -i \"s/^.*APP_DEBUG.*/define('APP_DEBUG',false);/\" index.php")
-	local("sed -i \"s/^.*APP_STATUS.*/define('APP_STATUS','%s');/\" index.php" % conf)
+    local("sed -i \"s/^.*APP_DEBUG.*/define('APP_DEBUG',false);/\" index.php")
+    local("sed -i \"s/^.*APP_STATUS.*/define('APP_STATUS','%s');/\" index.php" % conf)
+    # .html clear cache
+    local('/root/fabric/html.sh')
 
 def clearRuntime():
 	run('rm -rf ./Runtime/*')
