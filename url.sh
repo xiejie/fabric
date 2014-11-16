94f32af5bf2e8ea9c0e90c75c9e5cef617aac192
xiejie <214533579@qq.com>
2014-11-15 23:13:48 -0500

    url.sh

--- url.sh
+++ url.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
-lynx -dump www.baidu.com | sed -n '/^References/,$p' | \
-egrep -v "ftp://|javascript:|mailto:|news:|https://" | \
+if [ $# -ne 1 ]
+then
+    echo "Usage: $0 url"
+    exit 1
+fi
+
+lynx -dump $1 | sed -n '/^References/,$p' | \
+egrep -v "ftp://|javascript:|mailto:|news:|https://|^$|links:" | \
 tail -n +5 | awk '{print $2}' | cut -d\? -f1 
